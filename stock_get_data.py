493e84c0a6a7158397394d5d1d2423e7c81d6332
Kasper_Git <kasperkst@gmail.com>
2023-07-07 22:05:42 +0200

    get data from yfinance

---
+++ stock_get_data.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import pandas as pd
+import random
+
+# Define the stock symbols
+stocks = ["NVO", "AAPL", "MSFT", "TSLA", "^GSPC"]
+
+# Retrieve historical prices for each stock
+dfs = []
+for stock in stocks:
+    data = yf.download(stock, start="2004-01-01", end="2023-06-29")
+    df = pd.DataFrame(data)
+    df["Symbol"] = stock  # Add a column to identify the stock symbol
+    dfs.append(df)
+
+# Concatenate all the stock data into a single DataFrame
+historical_prices = pd.concat(dfs)
+
+# Define the file path
+#file_path = r"C:\Users\kaspe\Desktop\python_projects\financial_risk\historical_prices.xlsx"
+filename = "historical_prices2.xlsx"
+
+
+# Save DataFrame to Excel file
+historical_prices.to_excel(filename, index=False)
