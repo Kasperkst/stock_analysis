655f0182965a701a58984365d5f29a8925255520
Kasper_Git <kasperkst@gmail.com>
2023-07-07 22:45:39 +0200

    added a test for the class portfolio

--- test_stock.py
+++ test_stock.py
@@ -1,6 +1,7 @@
 import unittest
 import pandas as pd
 from stock import Stock
+from portfolio import Portfolio
 
 class TestStock(unittest.TestCase):
     def test_stock(self):
@@ -22,6 +23,30 @@ class TestStock(unittest.TestCase):
         returns = stock.calculate_returns()
         expected_return = (df['Close'].iloc[1] - df['Close'].iloc[0]) / df['Close'].iloc[0]
         self.assertEqual(returns, [expected_return])
+
+
+class TestPortfolio(unittest.TestCase):
+    def test_portfolio(self):
+        # Create a new portfolio
+        portfolio = Portfolio()
+
+        # Create a new stock
+        stock = Stock('NVO', 'Novo Nordisk')
+        stock.historical_prices = [4.124000072, 4.100999832, 4.097000122, 4.004000187]
+
+        # Add the stock to the portfolio
+        portfolio.add_stock(stock, 0.5)
+
+        # Check that the stock was added to the portfolio
+        self.assertEqual(portfolio.stocks[0], stock)
+        self.assertEqual(portfolio.allocation[0], 0.5)
+
+        # Remove the stock from the portfolio
+        portfolio.remove_stock(stock)
+
+        # Check that the portfolio is now empty
+        self.assertFalse(portfolio.stocks)
+        self.assertFalse(portfolio.allocation)
 
 if __name__ == '__main__':
     unittest.main()
